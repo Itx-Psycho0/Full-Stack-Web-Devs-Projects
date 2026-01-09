f315597c2a23c8f72b68fc451bebea02128bd2c1
Anurag Singh <anuragh3re@gmail.com>
2026-01-09 22:26:47 +0530

    setup backend

--- All-PROJECTS/MERN-THINKBOARD/backend/server.js
+++ All-PROJECTS/MERN-THINKBOARD/backend/server.js
@@ -3,6 +3,12 @@ import express from "express"
 
 const app = express()
 
+app.get("/api/notes",(req,res)=>{
+    res.status(200).send("You got 5 notes")
+})
+app.post("/api/notes",(req, res)=>{
+    res.status(201).send("Your note has created")
+})
 app.listen(5001,()=>{
     console.log("server started on PORT: 5001")
 })
